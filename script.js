6ff7cc934a912f1e60027a4b3338c9b6040e8c8d
Aseel <aseelalnassiry@gmail.com>
2022-02-14 14:29:43 -0800

    Button event added

--- script.js
+++ script.js
@@ -15,4 +15,22 @@ function operator(func, a, b) {
   return func(a, b);
 }
 
-//
+// Query selectors
+const displayValue = document.querySelector(".display");
+let numberButtons = document.querySelectorAll(".number-button");
+let operationButtons = document.querySelectorAll(".operation-button");
+
+// Event listener for the number buttons
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    displayValue.textContent += button.textContent;
+  });
+});
+// Event listener for the operation buttons
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    displayValue.textContent += button.textContent;
+  });
+});
+
+
