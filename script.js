70006208d2efd67cd87a219dcf516d4031629e36
Aseel <aseelalnassiry@gmail.com>
2022-02-15 13:14:33 -0800

    script and HTML finished, CSS modified to accomidate id's on some buttons

--- script.js
+++ script.js
@@ -2,35 +2,89 @@
 function add(a, b) {
   return a + b;
 }
+
 function subtract(a, b) {
   return a - b;
 }
+
 function multiply(a, b) {
   return a * b;
 }
+
 function devide(a, b) {
   return a / b;
 }
-function operator(func, a, b) {
-  return func(a, b);
+
+function operator(button, a, b) {
+  if (button === "+") {
+    return add(a, b);
+  } else if (button === "-") {
+    return subtract(a, b);
+  } else if (button === "*") {
+    return multiply(a, b);
+  } else {
+    return devide(a, b);
+  }
 }
+
+// Variables
+let firstValue = undefined;
+let secondValue = undefined;
+let answer = undefined;
+let currentOperation = undefined;
 
 // Query selectors
 const displayValue = document.querySelector(".display");
 let numberButtons = document.querySelectorAll(".number-button");
 let operationButtons = document.querySelectorAll(".operation-button");
+let clearButton = document.querySelector("#clear-button");
+let equalsButton = document.querySelector("#equals-button");
 
 // Event listener for the number buttons
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    displayValue.textContent += button.textContent;
+    if (!answer) {
+      displayValue.textContent += button.textContent;
+    } else {
+      displayValue.textContent = "";
+      firstValue = answer;
+      answer = undefined;
+      displayValue.textContent += button.textContent;
+    }
   });
 });
+
+// Event listner for the clear button
+clearButton.addEventListener("click", () => {
+  firstValue = undefined;
+  secondValue = undefined;
+  answer = undefined;
+  displayValue.textContent = "";
+});
+
 // Event listener for the operation buttons
 operationButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    displayValue.textContent += button.textContent;
+    if (firstValue === undefined) {
+      firstValue = parseFloat(displayValue.textContent);
+      displayValue.textContent = "";
+      currentOperation = button;
+    } else {
+      secondValue = parseFloat(displayValue.textContent);
+      answer = operator(currentOperation.textContent, firstValue, secondValue);
+      displayValue.textContent = answer;
+      currentOperation = button;
+    }
   });
 });
 
-
+// Event listner for the Equals button
+equalsButton.addEventListener("click", () => {
+  if (firstValue !== undefined) {
+    secondValue = parseFloat(displayValue.textContent);
+    answer = operator(currentOperation.textContent, firstValue, secondValue);
+    displayValue.textContent = answer;
+    firstValue = undefined;
+    secondValue = undefined;
+  }
+});
